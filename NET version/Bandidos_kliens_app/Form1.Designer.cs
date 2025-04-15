920c972fbf8e4602ce70033f842fc13126cd9f88
Lóránt Seszták <lorant.sesztak@stud.uni-corvinus.hu>
2025-04-15 17:33:20 +0200

    Első fejlesztés, a fő Form és a UserControl kialakítása.

--- NET version/Bandidos_kliens_app/Form1.Designer.cs
+++ NET version/Bandidos_kliens_app/Form1.Designer.cs
@@ -28,20 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1482, 1186);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1500, 838);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(404, 98);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1500, 962);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(404, 98);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1646, 1065);
+            this.ClientSize = new System.Drawing.Size(1916, 1210);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Bandidos";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- NET version/Bandidos_kliens_app/Form1.cs
+++ NET version/Bandidos_kliens_app/Form1.cs
@@ -16,5 +16,27 @@ namespace Bandidos_kliens_app
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Panel tartalmának törlése
+            panel1.Controls.Clear();
+
+            // UserControl1 példányosítása és hozzáadása a panelhez
+            UserControl1 userControl1 = new UserControl1();
+            userControl1.Dock = DockStyle.Fill; // Teljes kitöltés a panelben
+            panel1.Controls.Add(userControl1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Panel tartalmának törlése
+            panel1.Controls.Clear();
+
+            // UserControl2 példányosítása és hozzáadása a panelhez
+            UserControl2 userControl2 = new UserControl2();
+            userControl2.Dock = DockStyle.Fill; // Teljes kitöltés a panelben
+            panel1.Controls.Add(userControl2);
+        }
     }
 }
